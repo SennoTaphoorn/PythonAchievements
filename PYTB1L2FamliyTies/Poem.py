38fe991b595a34ba678ab3b6913c315e0b9e5712
SennoTaphoorn <29040@ma-web.nl>
2020-10-22 15:26:48 +0200

    Bestanden toegevoegd

---
+++ PYTB1L2FamliyTies/Poem.py
@@ -0,0 +1,8 @@
+
+Naam = 'Senno Taphoorn'
+Leeftijd = 17
+Plaats = 'Abcoude'
+Afstand = 15 KM
+
+print("Mijn naam is " + Naam + " ik ben " + str(Leeftijd) + " jaar oud en woon in " + Plaats + ". Ik woon " + str(Afstand) + " kilometer van het Mediacollege. ")
+print(type(Plaats))
