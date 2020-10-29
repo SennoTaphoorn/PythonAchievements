6b83e2d70deeefa1c33c062eba936b4e3c3cbf65
SennoTaphoorn <29040@ma-web.nl>
2020-10-29 21:37:24 +0100

    code veranderd

--- PYTB1L2PoemPrinting/Poem.py
+++ PYTB1L2PoemPrinting/Poem.py
@@ -1,5 +1,7 @@
-print("Min naam is Senno Taphoorn. 2002 is the year i was born.") 
-print("Ik houd van gamen elke dag. en ik houd ook veel van een lach.")
-print("Ik speel games en kijk films met plezier. En hoop het heel leuk te hebben hier. Met bier.")
-print("Ik studeer gamedevelopment. En hoop dat ik daar ooit goed in bent.")
-print("Ik wens iedereen veel geluk op deze opleiding. Ookal zorgen jullie voor veel afleiding")
+line1 = ("Mijn naam is Senno Taphoorn. 2002 is the year i was born\n")
+line2 = ("Ik houd van gamen elke dag. en ik houd ook veel van een lach\n")
+line3 = ("Ik speel games en kijk films met plezier. En hoop het heel leuk te hebben hier. Met bier\n")
+line4 = ("Ik studeer gamedevelopment. En hoop dat ik daar ooit goed in bent\n")
+line5 = ("Ik wens iedereen veel geluk op deze opleiding. Ookal zorgen jullie voor veel afleiding\n")
+
+print (line1,line2,line3,line4,line5)
