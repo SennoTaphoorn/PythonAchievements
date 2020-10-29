76dd46c61af34b5941525cae6bc334958fd3cff5
SennoTaphoorn <29040@ma-web.nl>
2020-10-29 11:34:10 +0100

    verkeerd bastand gefixt

---
+++ PYTB1L2FamliyTies/FamilyTies.py
@@ -0,0 +1,5 @@
+
+Naam = 'Senno Taphoorn'
+Leeftijd = 17
+Plaats = 'Abcoude'
+Afstand = 15 KM
--- PYTB1L2FamliyTies/Poem.py
+++
@@ -1,8 +0,0 @@
-
-Naam = 'Senno Taphoorn'
-Leeftijd = 17
-Plaats = 'Abcoude'
-Afstand = 15 KM
-
-print("Mijn naam is " + Naam + " ik ben " + str(Leeftijd) + " jaar oud en woon in " + Plaats + ". Ik woon " + str(Afstand) + " kilometer van het Mediacollege. ")
-print(type(Plaats))
